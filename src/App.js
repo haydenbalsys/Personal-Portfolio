d5bb437a710b1c1ab8decd73348ce97392a3b995
Hayden Balsys <74757315+haydenbalsys@users.noreply.github.com>
2023-05-12 17:18:52 -0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import AboutPage from "./components/pages/AboutPage";
+import AboutPage from "./components/pages/AboutPage";
+import ExperiencePage from "./components/pages/ExperiencePage";
+import EducationPage from "./components/pages/EducationPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <AboutPage />
+      <ExperiencePage />
+      <EducationPage />
+    </main>
   );
 }
-
-export default App;

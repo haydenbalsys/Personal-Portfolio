89b7270943957d501e0de47475c30e16a0dc008e
Hayden Balsys <74757315+haydenbalsys@users.noreply.github.com>
2023-09-22 17:20:36 -0700

    email.js

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -1,66 +1,72 @@
-import React, { useState } from 'react';
-import { db } from '../../firebase';
-import { collection, addDoc } from "firebase/firestore";
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import './ContactPage.css';
 
-const ContactForm = () => {
-  const defaultData = {
-    name: '',
-    email: '',
-    message: ''
-  }
+const ContactPage = () => {
+  const form = useRef();
+  const [submitted, setSubmitted] = useState(false);
 
-  const [data, setData] = useState(defaultData);
-
-  const submitForm = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    try {
-      const docRef = await addDoc(collection(db, "form-submissions"), {
-        name: data.name,
-        email: data.email,
-        message: data.message
-      });
-      alert("Form submitted successfully!");
-      setData(defaultData);
-    } catch (error) {
-      alert("Error submitting form!");
-    }
-  }
 
-  const handleChange = e => {
-    setData({ ...data, [e.target.name]: e.target.value });
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then((result) => {
+        console.log(result.text);
+        console.log("Message sent successfully!");
+        setSubmitted(true); // Set the submitted state to true
+        resetForm(); // Clear the form fields
+        setTimeout(() => {
+          setSubmitted(false); // Clear the "submitted successfully" message after a few seconds
+        }, 3000); // Adjust the duration (in milliseconds) as needed
+      })
+      .catch((error) => {
+        console.error(error.text);
+      });
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    alert(`Submitting form with data: ${JSON.stringify(formData)}`);
+  const resetForm = () => {
+    // Reset the form fields to their initial state
+    form.current.reset();
   };
 
   return (
     <div className='main'>
       <h1>Contact me!</h1>
-      <p className='text'>Ask me anything and I'll write back as soon as possible. Thanks!</p>
-      <form onSubmit={handleSubmit}>
+      <p className='text'>
+        Ask me anything and I'll write back as soon as possible. Thanks!
+      </p>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="form-row">
           <div className="form-field">
             <label>Name:</label>
-            <input type="text" name="name" value={data.name} onChange={handleChange} required />
+            <input type="text" name="user_name" required />
           </div>
           <div className="form-field">
             <label>Email:</label>
-            <input type="email" name="email" value={data.email} onChange={handleChange} required />
+            <input type="email" name="user_email" required />
           </div>
         </div>
         <div className="form-field">
           <label>Message:</label>
-          <textarea name="message" rows="5" value={data.message} onChange={handleChange} required />
+          <textarea name="message" rows="5" required />
         </div>
         <div className="form-field">
           <input type="submit" value="Send" />
         </div>
       </form>
+      {submitted && (
+        <div className='success-message'>
+          Submitted successfully!
+        </div>
+      )}
     </div>
   );
 };
 
-export default ContactForm;
+export default ContactPage;
